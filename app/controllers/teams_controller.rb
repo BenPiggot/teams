a9bf22491975ca3d6c0f0118ad88fb8623d51a8d
Ben <wbpiggot@gmail.com>
2015-04-27 21:08:41 -0700

    Adding update and delete methods

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -4,22 +4,44 @@ class TeamsController < ApplicationController
     @teams = Team.all
   end
 
+
   def new
     @team = Team.new
   end
+
 
   def create
     @team = Team.create(team_params)
     redirect_to @team
   end
 
+
   def show
     @team = Team.find(params[:id])
   end
 
-  def delete
-    @team = Team.destroy(params[:id])
+
+  def destroy
+    @team = Team.find(params[:id])
+    @team.destroy
+    redirect_to teams_path
   end
+
+
+  def edit
+    @team = Team.find(params[:id])
+  end
+
+
+  def update
+  @team = Team.find(params[:id])
+    if @team.update(team_params)
+      redirect_to @team
+    else
+      render 'edit'
+    end
+  end
+
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
- resources :teams, :only => [:index, :create, :new, :show]
+ resources :teams
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
